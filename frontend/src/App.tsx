2c32f6c19d9481925e340762583b9bb8848166c5
jamescutts0801 <jamescutts0801@gmail.com>
2025-05-28 09:51:36 +0100

    Linked the frontend application to  the spring boot application.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  useEffect(() => {
+    fetch('/upload-recipe/success')
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-intro">
+          <h2>JUG List</h2>
+        </div>
       </header>
     </div>
   );
