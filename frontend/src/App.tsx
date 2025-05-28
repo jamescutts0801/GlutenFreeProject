080a45dc472a9142a536a50a404c09e0415121cd
jamescutts0801 <jamescutts0801@gmail.com>
2025-05-28 10:02:25 +0100

    Removed the logo from the starter application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 const App = () => {
@@ -11,7 +10,6 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <div className="App-intro">
           <h2>JUG List</h2>
         </div>
