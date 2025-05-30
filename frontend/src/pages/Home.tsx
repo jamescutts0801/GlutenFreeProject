a704366f0aed0b92499a5a7df0fdf2a667cb19d2
jamescutts0801 <jamescutts0801@gmail.com>
2025-05-30 15:34:30 +0100

    Updated the frontend code so all recipe names are pulled onto the homepage.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
-    return (
+
+  const [recipes, setRecipes] = useState([{
+    recipeName: "",
+    recipeMethod: ""
+  }]);
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://localhost:8080/recipes",
+    }).then(res => {
+      setRecipes(res.data)
+    });
+  })
+
+  return (
     <div>
-     <h1>Home Page</h1>
-     <p>Welcome to the Home Page!</p>
-     <button><Link to="/add-recipe">Add Recipe</Link></button>
-   </div>
-    )
+      <button><Link to="/add-recipe">Add Recipe</Link></button>
+      <div>
+        {recipes.map((recipe: { recipeName: string }) =>
+          <div>
+            {recipe.recipeName}
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Home
