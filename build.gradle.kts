97c3d5d9a9adfb1a469f13a082993b18410053d0
jamescutts0801 <jamescutts0801@gmail.com>
2025-05-30 08:23:32 +0100

    Replaced h2 database with postgre

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,16 +18,23 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.5.0")
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
+    implementation("org.springframework.boot:spring-boot-starter-web:3.5.0")
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core
     implementation("com.fasterxml.jackson.core:jackson-core:2.19.0")
 
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-devtools
+    implementation("org.springframework.boot:spring-boot-devtools:3.5.0")
 
-    runtimeOnly("com.h2database:h2")
+    // https://mvnrepository.com/artifact/org.postgresql/postgresql
+    implementation("org.postgresql:postgresql:42.7.6")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-test
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.5.0")
+    // https://mvnrepository.com/artifact/org.junit.platform/junit-platform-launcher
+    runtimeOnly("org.junit.platform:junit-platform-launcher:1.12.2")
 }
 
 tasks.withType<Test> {
