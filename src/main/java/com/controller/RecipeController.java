cddb396269762f0935225a60ffb41e9cd785d6f3
jamescutts0801 <jamescutts0801@gmail.com>
2025-05-30 09:06:06 +0100

    Added a repo so the recipe is saved to the database.

--- src/main/java/com/controller/RecipeController.java
+++ src/main/java/com/controller/RecipeController.java
@@ -3,6 +3,8 @@ package com.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.model.Recipe;
+import com.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RecipeController {
 
+    @Autowired
+    private RecipeRepository recipeRepo;
     private final ObjectMapper mapper = new ObjectMapper();
 
     @RequestMapping("/upload-recipe")
     public void recipeUploadSuccess(@RequestBody String recipeInputted) throws JsonProcessingException {
         Recipe recipe = mapper.readValue(recipeInputted, Recipe.class);
+        recipeRepo.save(recipe);
     }
 }
